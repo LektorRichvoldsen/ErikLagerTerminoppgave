d7800d5ec78c352c8242fef75a8c7113eb55d6e6
LektorRichvoldsen <90402122+LektorRichvoldsen@users.noreply.github.com>
2022-01-05 13:42:31 +0100

    Lagt til klikkespill. Fjernet Arvids shenanigans

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Her kommer det kode");
+var knappEl = document.getElementById("klikk");
+knappEl.addEventListener("click", oppdaterKlikk);
+
+var resultatEl = document.getElementById("resultat");
+
+var antallKlikk = 0;
+function oppdaterKlikk(e) {
+    antallKlikk++;
+
+    resultatEl.innerHTML = "Wow, du har trykket " + antallKlikk + " ganger!";
+}
