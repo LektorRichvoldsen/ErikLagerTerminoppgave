f9b316ae1b6142107235866d68b12e9667402cbf
LektorRichvoldsen <90402122+LektorRichvoldsen@users.noreply.github.com>
2022-01-06 14:39:52 +0100

    Lagt til innlogging og en del dokumentasjon

--- php/config.php
+++ php/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_USERNAME', 'vanlig_bruker');
+define('DB_PASSWORD', 'test1234');
+define('DB_NAME', 'terminoppgave');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
